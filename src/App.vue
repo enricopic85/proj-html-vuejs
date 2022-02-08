613c29e2f48b66a419688c00884eda6be187dbfb
enricopic85 <enricodev85@gmail.com>
2022-02-08 11:20:37 +0100

    creazione macro componenti e variabili scss

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
-   
+    <header-app/>
+    <main-app/>
+    <footer-app/>
   </div>
 </template>
 
 <script>
+import FooterApp from './components/FooterApp.vue'
+import HeaderApp from './components/HeaderApp.vue'
+import MainApp from './components/MainApp.vue'
+
 
 
 export default {
   name: 'App',
   components: {
+    HeaderApp,
+    MainApp,
+    FooterApp
    
   }
 }
 </script>
 
 <style lang="scss">
-
+  @import './style/main.scss';
 </style>
