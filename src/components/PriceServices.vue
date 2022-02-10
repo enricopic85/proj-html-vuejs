40b841be86265946455ca3f87a204b27f2c9178a
enricopic85 <enricodev85@gmail.com>
2022-02-10 11:30:35 +0100

    overlay

--- src/components/PriceServices.vue
+++ src/components/PriceServices.vue
@@ -3,10 +3,14 @@
         <text-big-dream/>    
         <help-you/>
         <div class="container-price">
-            <div class="card-price">
-                
+            <div class="card-price1">
+                <div class="price-overlay"></div>
+                <div class="price">
+                    <h4>$40.oo</h4>
+                    <p>Learning write as a </p>
+                    <p>professional author</p>
+                </div>
             </div>
-            <div class="card-price1"></div>
             <div class="card-price2"></div>
             <div class="card-price3"></div>
             <div class="card-price4"></div>
@@ -37,8 +41,36 @@ export default {
                width: 280px;
                height: 350px;
                border: 1px solid black;
-                border-radius: 5px;
-                 background-size: cover;
+               border-radius: 5px;
+               background-size: cover;
+               display: flex;
+               flex-direction: column;
+               justify-content: flex-end;
+               position: relative;
+               .price-overlay{
+                   position: absolute;
+                   top: 0;
+                   left: 0;
+                   background: black;
+                   opacity: 0.2;
+                   z-index: 1;
+                   width: 100%;
+                   height: 100%;
+               }
+               .price{
+                   padding-left: 20px;
+                   padding-bottom: 20px;
+                   position: absolute;
+                   z-index: 2;
+                   h4{
+                       color: $jungle-green-v;
+                   }
+                   p{
+                       font-size: 14px;
+                       color: $white;
+                       font-weight: 800;
+                   }
+               }
            }
            .card-price1,.card-price4{
                background-image: url('/img/stock-full-hd-03-443x600.jpg');
